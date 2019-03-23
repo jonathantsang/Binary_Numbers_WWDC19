f1fcb54181705da17a8e633f9bc73acb8898cf97
jonathantsang <j26tsang@gmail.com>
2019-03-23 14:00:24 -0400

    more write up

--- Learn Binary Numbers.playground/Pages/2. NumtoBase2.xcplaygroundpage/Contents.swift
+++ Learn Binary Numbers.playground/Pages/2. NumtoBase2.xcplaygroundpage/Contents.swift
@@ -119,12 +119,12 @@ let screenWidth = screenSize.width
 let screenHeight = screenSize.height
 
 // Create labels
-let numberLabel = UILabel(frame: CGRect(x: screenWidth / 2, y: 100, width: 300, height: 50))
+let numberLabel = UILabel(frame: CGRect(x: screenWidth / 2, y: 100, width: 300, height: 60))
 numberLabel.font = UIFont.systemFont(ofSize: 50)
 numberLabel.textColor = UIColor.purple
 numberLabel.textAlignment = .center
 numberLabel.text = "0"
-let binaryLabel = UILabel(frame: CGRect(x: screenWidth / 2, y: screenHeight - 500, width: 300, height: 50))
+let binaryLabel = UILabel(frame: CGRect(x: screenWidth / 2, y: screenHeight - 500, width: 300, height: 60))
 binaryLabel.font = UIFont.systemFont(ofSize: 50)
 binaryLabel.textColor = UIColor.purple
 binaryLabel.textAlignment = .center
--- Learn Binary Numbers.playground/Pages/3. Base2toNum.xcplaygroundpage/Contents.swift
+++ Learn Binary Numbers.playground/Pages/3. Base2toNum.xcplaygroundpage/Contents.swift
@@ -6,9 +6,28 @@
 
 /*:
  - - -
- ## What is a binary number?
- Now that we know how to convert from decimal numbers to binary numbers, how do we do it in the other direction?
+ ## Conversion from Binary to Decimal
  
+ - callout(Goal):
+ Learn about the differences between a decimal number and a binary number.
+ 
+ Now that we know how to convert from decimal numbers to binary numbers, how do we do it in the other direction?
+ - - -
+ 
+ - Example:
+ How is 23 represented in base 10?
+ 
+ 2 * 10 + 3 * 10 = 23
+ 
+ Where 23 contains two tens and three ones to form 23.
+ 
+ We can also represent 23 using powers of 10.
+ 
+ 2 * 10^1 + 3 * 10^0 = 23
+ 
+ We notice here that the ones digit 3 is multiplied by 10^0 and the tens digit 2 is multiplied by 10^1, and each increasing column in the number would be multiplied by 10 to an increasing exponent.
+ 
+ - - -
  
  - - -
 
